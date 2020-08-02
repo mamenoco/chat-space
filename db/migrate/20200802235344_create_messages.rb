ae93f1567a02f20e2ee02f7c0c67b1bf22582d8d
mamenoco <mamenoco@gmail.com>
2020-08-03 08:55:54 +0900

    add message migration

--- db/migrate/20200802235344_create_messages.rb
+++ db/migrate/20200802235344_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.
+      t.string :content
+      t.string :name
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
