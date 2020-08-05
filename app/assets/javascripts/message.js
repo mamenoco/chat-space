7a2f73e90dad0134dbfd04f4b97ec96ab792999e
mamenoco <mamenoco@gmail.com>
2020-08-05 10:54:05 +0900

    add animate

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -54,6 +54,7 @@ $(function(){
           insertHTML += buildHTML(message)
         });
         $('.MessageField').append(insertHTML);
+        $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
       }
     })
     .fail(function() {
